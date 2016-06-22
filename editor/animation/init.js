7c80d75868555fef8e3acd9803596fd0faf9bd6c
Oleksandr Liabakh <oleksandrliabakh@Oleksandrs-MacBook.local>
2016-06-22 19:36:59 +0300

    nonUniqueElements

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -6,6 +6,10 @@ requirejs(['ext_editor_io', 'jquery_190'],
         var $tryit;
 
         var io = new extIO({
+            functions: {
+                js: 'nonUniqueElements',
+                python: 'checkio'
+            },
             tryit:function (this_e) {
                 $tryit = this_e.extSetHtmlTryIt(this_e.getTemplate('tryit')).find(".tryit-content");
             
