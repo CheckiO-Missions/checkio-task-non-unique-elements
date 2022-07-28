1fb6566b35a1e8a8f1b965beffe241cd65bfea7a
Oleksandr Zozulia <freemanlex@gmail.com>
2022-07-28 17:30:58 +0300

    next API+UK hints
    + UK hints for JS
    + adding EN short descriptions for missing languages

--- verification/referee.py
+++ verification/referee.py
@@ -1,23 +1,21 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
-from checkio.referees.io import CheckiOReferee
+from checkio.referees.io_template import CheckiOReferee
 
 from tests import TESTS
-
-cover = """def cover(func, data):
-    return func(data)
-"""
 
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
-        cover_code={
-            'python-27': cover,
-            'python-3': cover
-        },
         function_name={
             "python": "checkio",
             "js": "nonUniqueElements"
+        },
+        cover_code={
+            'python-3': {},
+            'js-node': {
+                # "dateForZeros": True,
+            }
         }
     ).on_ready)
