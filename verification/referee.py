f7f79cddce16bf63bc7bbe20ad08d0611cacb41b
Oleksandr Zozulia <freemanlex@gmail.com>
2023-01-26 18:31:37 +0000

    upd

--- verification/referee.py
+++ verification/referee.py
@@ -1,6 +1,7 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io_template import CheckiOReferee
+from checkio.referees.checkers import to_list
 
 from tests import TESTS
 
@@ -8,6 +9,7 @@ api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
+        checker=to_list,
         function_name={
             "python": "checkio",
             "js": "nonUniqueElements"
